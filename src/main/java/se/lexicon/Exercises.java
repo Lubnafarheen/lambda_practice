96fd22d43c8841789cce2807b0a193fb54b19810
lubna <lubnafarheensweden@gmail.com>
2022-12-01 13:45:39 +0100

    implemented exercises

--- src/main/java/se/lexicon/Exercises.java
+++ src/main/java/se/lexicon/Exercises.java
@@ -5,6 +5,8 @@ import se.lexicon.model.Gender;
 import se.lexicon.model.Person;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.Comparator;
 
 public class Exercises {
 
@@ -13,7 +15,7 @@ public class Exercises {
     /*
        1.	Find everyone that has firstName: “Erik” using findMany().
     */
-    public static void exercise1(String message){
+    public static void exercise1(String message) {
         System.out.println(message);
         //Write your code here
         storage.findMany(person -> person.getFirstName().equals("Erik")).forEach(System.out::println);
@@ -23,7 +25,7 @@ public class Exercises {
     /*
         2.	Find all females in the collection using findMany().
      */
-    public static void exercise2(String message){
+    public static void exercise2(String message) {
         System.out.println(message);
         storage.findMany(females -> females.getGender().equals(Gender.FEMALE)).forEach(System.out::println);
         System.out.println("----------------------");
@@ -32,7 +34,7 @@ public class Exercises {
     /*
         3.	Find all who are born after (and including) 2000-01-01 using findMany().
      */
-    public static void exercise3(String message){
+    public static void exercise3(String message) {
         System.out.println(message);
         storage.findMany(person -> person.getBirthDate().isAfter(LocalDate.parse("1999-01-01"))).forEach(System.out::println);
         System.out.println("----------------------");
@@ -41,9 +43,9 @@ public class Exercises {
     /*
         4.	Find the Person that has an id of 123 using findOne().
      */
-    public static void exercise4(String message){
+    public static void exercise4(String message) {
         System.out.println(message);
-        System.out.println(storage.findOne(person -> person.getId()==123));
+        System.out.println(storage.findOne(person -> person.getId() == 123));
         System.out.println("----------------------");
 
     }
@@ -52,9 +54,9 @@ public class Exercises {
         5.	Find the Person that has an id of 456 and convert to String with following content:
             “Name: Nisse Nilsson born 1999-09-09”. Use findOneAndMapToString().
      */
-    public static void exercise5(String message){
+    public static void exercise5(String message) {
         System.out.println(message);
-        System.out.println(storage.findOneAndMapToString(person -> person.getId() == 456, person -> person.getFirstName()+" "+person.getLastName()+""+
+        System.out.println(storage.findOneAndMapToString(person -> person.getId() == 456, person -> person.getFirstName() + " " + person.getLastName() + "" +
                 "born" + person.getBirthDate()));
         System.out.println("----------------------");
     }
@@ -62,7 +64,7 @@ public class Exercises {
     /*
         6.	Find all male people whose names start with “E” and convert each to a String using findManyAndMapEachToString().
      */
-    public static void exercise6(String message){
+    public static void exercise6(String message) {
         System.out.println(message);
         storage.findManyAndMapEachToString(
                 person -> person.getGender() == Gender.MALE && person.getFirstName().startsWith("E"),
@@ -74,52 +76,50 @@ public class Exercises {
         7.	Find all people who are below age of 10 and convert them to a String like this:
             “Olle Svensson 9 years”. Use findManyAndMapEachToString() method.
      */
-    public static void exercise7(String message){
+    public static void exercise7(String message) {
         System.out.println(message);
-        //Write your code here
-        // TODO: exercise7
-
+        storage.findManyAndMapEachToString(
+                person -> Period.between(person.getBirthDate(), LocalDate.now()).getYears() < 10,
+                person -> person.getFirstName() + " " + person.getLastName() + " " + Period.between(person.getBirthDate(), LocalDate.now()).getYears() + " years"
+        ).forEach(System.out::println);
         System.out.println("----------------------");
     }
 
     /*
         8.	Using findAndDo() print out all people with firstName “Ulf”.
      */
-    public static void exercise8(String message){
+    public static void exercise8(String message) {
         System.out.println(message);
-        storage.findAndDo(person -> person.getFirstName().equals("Ulf"), System.out::println );
+        storage.findAndDo(person -> person.getFirstName().equals("Ulf"), System.out::println);
         System.out.println("----------------------");
     }
 
     /*
         9.	Using findAndDo() print out everyone who have their lastName contain their firstName.
      */
-    public static void exercise9(String message){
+    public static void exercise9(String message) {
         System.out.println(message);
-        //Write your code here
-        // TODO: exercise9
-
+        storage.findAndDo(person -> person.getLastName().contains(person.getFirstName()), System.out::println);
         System.out.println("----------------------");
     }
 
     /*
         10.	Using findAndDo() print out the firstName and lastName of everyone whose firstName is a palindrome.
      */
-    public static void exercise10(String message){
+    public static void exercise10(String message) {
         System.out.println(message);
-        //Write your code here
-        // TODO: exercise10
-
+        storage.findAndDo(person -> person.getFirstName().equalsIgnoreCase(new StringBuilder(person.getFirstName()).reverse().toString()),
+                person -> System.out.println(person.getFirstName() + "" + person.getLastName()));
         System.out.println("----------------------");
     }
 
     /*
         11.	Using findAndSort() find everyone whose firstName starts with A sorted by birthdate.
      */
-    public static void exercise11(String message){
+    public static void exercise11(String message) {
         System.out.println(message);
-        //Write your code here
-        // TODO: exercise11
+        storage.findAndSort(person -> person.getFirstName().startsWith("A"), Comparator.comparing(Person::getBirthDate)
+        ).forEach(System.out::println);
 
         System.out.println("----------------------");
     }
@@ -127,10 +127,12 @@ public class Exercises {
     /*
         12.	Using findAndSort() find everyone born before 1950 sorted reversed by lastest to earliest.
      */
-    public static void exercise12(String message){
+    public static void exercise12(String message) {
         System.out.println(message);
-        //Write your code here
-        // TODO: exercise12
+        storage.findAndSort(
+                person -> person.getBirthDate().getYear() < 1950,
+                Comparator.comparing(Person::getBirthDate).reversed()
+        ).forEach(System.out::println);
 
         System.out.println("----------------------");
     }
@@ -138,11 +140,11 @@ public class Exercises {
     /*
         13.	Using findAndSort() find everyone sorted in following order: lastName > firstName > birthDate.
      */
-    public static void exercise13(String message){
+    public static void exercise13(String message) {
         System.out.println(message);
-        //Write your code here
-        // TODO: exercise13
-
+        storage.findAndSort(
+                Comparator.comparing(Person::getLastName).thenComparing(Person::getFirstName).thenComparing(Person::getBirthDate)
+        ).forEach(System.out::println);
         System.out.println("----------------------");
     }
 }
